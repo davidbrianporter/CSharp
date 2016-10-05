7db3987f51c72b14146308d0cdeb804c7a359a37
David Porter <davidbrianporter@gmail.com>
2016-10-05 16:29:13 -0400

    finished interface assignment

--- Drawing/Drawing/Circle.cs
+++ Drawing/Drawing/Circle.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
 
 namespace Drawing
 {
-    class Circle
+    class Circle : IDraw, IColor 
     {
+        private int diameter;
+        private int locX = 0, locY = 0;
+        private Ellipse circle = null;
+
+        void IDraw.SetLocation(int xCoord, int yCoord)
+        {
+            this.locX = xCoord;
+            this.locY = yCoord;
+        }
+
+        void IDraw.Draw(Canvas canvas)
+        {
+            if (this.circle != null)
+            {
+                canvas.Children.Remove(this.circle);
+            }
+            else
+            {
+                this.circle = new Ellipse();
+            }
+            this.circle.Height = this.diameter;
+            this.circle.Width = this.diameter;
+            Canvas.SetTop(this.circle, this.locY);
+            Canvas.SetLeft(this.circle, this.locX);
+            canvas.Children.Add(this.circle);
+        }
+
+        void IColor.SetColor(Color color)
+        {
+            if (this.circle != null)
+            {
+                SolidColorBrush brush = new SolidColorBrush(color);
+                this.circle.Fill = brush;
+            }
+        }
+
+        public Circle(int diameter)
+        {
+            this.diameter = diameter;
+        }
     }
+
+    
+
 }
