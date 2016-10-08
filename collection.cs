5bee94a942c0620004b7021d2bd694850b6a5219
David Porter <davidbrianporter@gmail.com>
2016-10-07 20:14:25 -0400

    created collection and iterated over

--- collection.cs
+++ collection.cs
@@ -1,4 +1,26 @@
 using System;
 using System.Collections.Generic;
 
-//todo actually make
+public class run
+{
+
+    public static void Main()
+    {
+        //should the collection go in the main method? or a new collection class
+
+          List<int> numbers = new List<int>();
+
+              int[] arr = new int[3]{10,9,2};
+	      
+	      foreach (int num in arr)
+              {
+        // where does the Add method come from?
+                  numbers.Add(num);
+		  Console.WriteLine(number);
+              }
+
+        Console.WriteLine("hi");
+
+    }
+
+}
