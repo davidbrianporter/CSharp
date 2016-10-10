94cabadfeae42f28c3f7f1045d9123125a09a498
David Porter <davidbrianporter@gmail.com>
2016-10-10 14:26:45 -0400

    starting collections

--- collection.cs
+++ collection.cs
@@ -6,21 +6,17 @@ public class run
 
     public static void Main()
     {
-        //should the collection go in the main method? or a new collection class
-
-          List<int> numbers = new List<int>();
-
-              int[] arr = new int[3]{10,9,2};
+        List<int> numbers = new List<int>();
+        int[] arr = new int[3]{10,9,2};
 	      
-	      foreach (int num in arr)
-              {
-        // where does the Add method come from?
-                  numbers.Add(num);
-		  Console.WriteLine(number);
-              }
+	foreach (int num in arr)
+        {
+            numbers.Add(num);
+	}
 
-        Console.WriteLine("hi");
-
+        foreach (int n in numbers)
+	{
+            Console.WriteLine(n);
+	}
     }
-
 }
---
+++ generics.cs
@@ -0,0 +1,19 @@
+// this will demonstrate generics
+
+using System;
+
+namespace gennygen
+{
+class generics
+{
+    public static void Main()
+    {
+        Console.WriteLine("I will create a generic, ");
+    }
+}
+}
+
+
+
+
+// Generics are...
