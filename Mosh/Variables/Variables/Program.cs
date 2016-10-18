087740b45849d45fd11b6d7a8c5021898407d84d
David Porter <davidbrianporter@gmail.com>
2016-10-18 12:04:59 -0400

    end of Variables lecture

--- Mosh/Variables/Variables/Program.cs
+++ Mosh/Variables/Variables/Program.cs
@@ -10,6 +10,10 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+             Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            const float Pi = 3.14f;
+            Console.WriteLine(Pi);
         }
     }
 }
