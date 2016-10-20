d351fce67db248acc26f666bdd5466e609622226
David Porter <davidbrianporter@gmail.com>
2016-10-20 16:26:52 -0400

    working on arrays and objects

---
+++ test.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace one 
+{
+    public class test
+    {
+        public static void Main()
+	{
+	    var ar = new int[3,3]
+	    {
+	        {1, 2, 3},
+	        {4, 5, 6},
+	        {7, 8, 9}
+	    };
+	}
+    }
+} 
