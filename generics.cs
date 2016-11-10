82345a25e5dcd6b5e109b08cde7c74468b0d1a1c
David Porter <davidbrianporter@gmail.com>
2016-11-09 20:37:44 -0500

    created Generic class for testing

--- generics.cs
+++ generics.cs
@@ -4,11 +4,24 @@ using System;
 
 namespace gennygen
 {
-class generics
+
+public class GenericList<T>
+{
+    public void Add(T input) { }
+}
+
+class GenericRun
 {
     public static void Main()
     {
         Console.WriteLine("I will create a generic, ");
+        
+        GenericList<int> list1 = new GenericList<int>();
+
+        GenericList<string> list2 = new GenericList<string>();
+
+        list1.Add(7);
+        Console.WriteLine(list1);
     }
 }
 }
@@ -16,4 +29,4 @@ class generics
 
 
 
-// Generics are...
+// Generics are a way to allow for a class to use different types with the same methods from the generic class
