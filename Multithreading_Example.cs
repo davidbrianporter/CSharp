df101a4ad0d029ecf39099c7125307b5b6c6ae19
David Porter <davidbrianporter@gmail.com>
2017-01-11 15:57:26 -0500

    starting multithreading example

---
+++ Multithreading_Example.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("okie");
+        }
+    }
+}
